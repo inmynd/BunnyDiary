98e8437bac83b269597e47a0b35705543cf67a0f
Minah Park | Ari <inmynd.ca@gmail.com>
2025-08-22 01:16:21 +0900

    일기작성뷰에 데이터 연결

--- BunnyDiary/BunnyDiary/View/WriteView.swift
+++ BunnyDiary/BunnyDiary/View/WriteView.swift
@@ -6,8 +6,16 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct WriteView: View {
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var context
+    @State private var lineOne: String = ""
+    @State private var lineTwo: String = ""
+    @State private var lineThree: String = ""
+    @State private var showSave = false
+    
     var body: some View {
         VStack{
             Image("MainBunny")
@@ -15,7 +23,29 @@ struct WriteView: View {
                 .frame(width: 100, height: 100)
             Text("오늘의 감사한 일을 작성해주세요!")
                 .font(.Bunny35)
+                .padding()
+            DiaryTextField(text: $lineOne, placeholder: "한줄감사")
+            DiaryTextField(text: $lineTwo, placeholder: "두줄감사")
+            DiaryTextField(text: $lineThree, placeholder: "세줄감사")
             
+            SaveButton(
+                title: "세줄감사 저장하기",
+                isEnabled: .constant(!(lineOne.isEmpty || lineTwo.isEmpty || lineThree.isEmpty))
+            ) {
+                // 아래부터 SwiftData 저장로직
+                let item = ThanksDiary(line1: lineOne, line2: lineTwo, line3: lineThree, date: Date())
+                context.insert(item)
+                do {
+                    try context.save()
+                } catch {
+                    print("Save failed: \(error)")
+                }
+                showSave = true
+            }
+            .padding(.top)
+        }
+        .sheet(isPresented: $showSave) {
+            SaveView()
         }
     }
 }
