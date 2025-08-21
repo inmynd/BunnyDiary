e0fa8d2861edab76d0abdb7a95aa4084ce9df80f
Minah Park | Ari <inmynd.ca@gmail.com>
2025-08-22 01:17:00 +0900

    feat: 모달 카드뷰

--- BunnyDiary/BunnyDiary/View/CardView.swift
+++ BunnyDiary/BunnyDiary/View/CardView.swift
@@ -6,13 +6,61 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct CardView: View {
+    let item: ThanksDiary
+    @Environment(\.dismiss) private var dismiss
+    //\. dismiss 뷰닫기
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(.smileBunny)
+                .padding()
+            Text(Date().formatted(.dateTime.year().month().day().weekday(.wide).locale(Locale(identifier: "ko_KR"))))
+                .font(.Bunny40)
+            
+            VStack(alignment: .leading, spacing: 28) {
+                if !item.line1.isEmpty {
+                    HStack(alignment: .firstTextBaseline, spacing: 12) {
+                        Image("Carrot")
+                        Text(item.line1)
+                            .font(.Bunny30)
+                    }
+                }
+                if !item.line2.isEmpty {
+                    HStack(alignment: .firstTextBaseline, spacing: 12) {
+                        Image("Carrot")
+                        Text(item.line2)
+                            .font(.Bunny30)
+                    }
+                }
+                if !item.line3.isEmpty {
+                    HStack(alignment: .firstTextBaseline, spacing: 12) {
+                        Image("Carrot")
+                        Text(item.line3)
+                            .font(.Bunny30)
+                    }
+                }
+            }
+            .padding(24)
+            .frame(width: 353, height:295, alignment: .leading)
+            .background(RoundedRectangle(cornerRadius: 24).fill(Color("BunnyGray")))
+            
+            Button(action: { dismiss() }) {
+                Text("홈화면으로 가기")
+                    .font(.Bunny30)
+                    .foregroundStyle(.bunnyBlack)
+                    .frame(width: 353, height: 85)
+                    .background(.bunnyPink)
+                    .clipShape(.buttonBorder)
+            }
+            .padding(.top, 30)
+        }
+        .padding()
     }
 }
 
 #Preview {
-    CardView()
+    CardView(item: ThanksDiary(line1: "오늘도 맑음", line2: "아침 운동을 했다!", line3: "새벽예배에 다녀왔다!", date: Date()))
 }
