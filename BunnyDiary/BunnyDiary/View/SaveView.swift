51fe4d22dc3518fa6386cabca7f60ccf21661f24
Minah Park | Ari <inmynd.ca@gmail.com>
2025-08-22 01:18:29 +0900

    feat: 프로젝트 설정 등 수정

--- BunnyDiary/BunnyDiary/View/SaveView.swift
+++ BunnyDiary/BunnyDiary/View/SaveView.swift
@@ -7,9 +7,31 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct SaveView: View {
+    @State private var save: Bool = false
+    let delay: Double = 0.8
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            Image(save ? "SaveBunnyFill" : "SaveBunnyBlank")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 180)
+                .contentTransition(.opacity)
+
+            Text(save ? "세줄감사 저장 완료!" : "세줄감사 저장 중 ...")
+                .font(.Bunny40)
+                .foregroundStyle(.bunnyBlack)
+                .transition(.opacity)
+        }
+        .animation(.easeInOut(duration: 0.3), value: save)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                withAnimation { save = true }
+            }
+        }
     }
 }
 
