eefc6b8d93471e81766040dc0e6028c75ffb192a
Minah Park | Ari <inmynd.ca@gmail.com>
2025-08-22 01:17:29 +0900

    feat: 홈뷰에 데이터 연결

--- BunnyDiary/BunnyDiary/View/HomeView.swift
+++ BunnyDiary/BunnyDiary/View/HomeView.swift
@@ -6,14 +6,23 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct HomeView: View {
+    @Query(sort: \ThanksDiary.date, order: .reverse)
+    private var diaries: [ThanksDiary]
+    
     var body: some View {
+        let hasEntries = !diaries.isEmpty
+        let bunnyName = hasEntries ? "MainBunny" : "SmileBunny"
         VStack {
             Text(Date().formatted(.dateTime.year().month().day().weekday(.wide).locale(Locale(identifier: "ko_KR"))))
                 .font(.Bunny40)
-            Image(.mainBunny)
-            Text("오늘의 감사한 일을\n세줄로\n기록해보세요!")
+            Image(bunnyName)
+                .resizable()
+                .scaledToFit()
+                .padding(.all)
+            Text(hasEntries ? "오늘의 감사를\n함께 나눌 수 있어\n감사해요!" : "오늘의 감사한 일을\n세줄로\n기록해보세요!")
                 .multilineTextAlignment(.center)
                 .font(.Bunny40)
             HStack{
